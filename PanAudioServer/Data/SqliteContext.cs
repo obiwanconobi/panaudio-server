8d8eb5a3583ef445c118cbcc60384ebeed177329
obiwanconobi <connerpanaro@gmail.com>
2024-11-14 17:28:20 +0000

    env variables

--- PanAudioServer/Data/SqliteContext.cs
+++ PanAudioServer/Data/SqliteContext.cs
@@ -10,17 +10,17 @@ namespace PanAudioServer.Data
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-         //   var test = Environment.GetEnvironmentVariable("SqliteDB");
+            //   var test = Environment.GetEnvironmentVariable("SqliteDB");
             //  Console.WriteLine("DB location: " + test.ToString());
             //    var path = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase);
             //  string curdir = Directory.GetCurrentDirectory();
             //    string relativePath = @"Data\FoxessWebbus.db";
 
             // string test = string.Format("Data Source={0};", relativePath);
+            String test = Environment.GetEnvironmentVariable("SqliteDB");
 
 
-
-            optionsBuilder.UseSqlite(@"Data Source=Data//panaudio.db");
+            optionsBuilder.UseSqlite(test);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
