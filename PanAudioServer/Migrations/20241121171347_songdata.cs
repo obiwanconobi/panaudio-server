3af2b12fdbe93812fe122be058a0d96f83e84e1d
obiwanconobi <connerpanaro@gmail.com>
2024-11-21 17:14:07 +0000

    migration

---
+++ PanAudioServer/Migrations/20241121171347_songdata.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PanAudioServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class songdata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BitDepth",
+                table: "Songs",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BitRate",
+                table: "Songs",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Codec",
+                table: "Songs",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SampleRate",
+                table: "Songs",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BitDepth",
+                table: "Songs");
+
+            migrationBuilder.DropColumn(
+                name: "BitRate",
+                table: "Songs");
+
+            migrationBuilder.DropColumn(
+                name: "Codec",
+                table: "Songs");
+
+            migrationBuilder.DropColumn(
+                name: "SampleRate",
+                table: "Songs");
+        }
+    }
+}
